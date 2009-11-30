33dfe2c962b2fb4d37cd48b46f568d8a50df0dd8
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-11-30 11:25:53 -0600

    Push 1.0-a-1 to pypi.

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-googlecharts',
-    version = '1.0a1',
+    version = '1.0-alpha-1',
     description = 'Django template tags to generate charts using the Google chart API.',
+    long_description = open("README").read(),
     keywords = 'django charts templates',
     license = 'BSD',
     author = 'Jacob Kaplan-Moss',
